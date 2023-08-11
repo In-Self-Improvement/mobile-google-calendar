e0efd8a55db71009658821cd80914e5fe11e6c9c
Bong <borobong230@gmail.com>
2023-08-11 23:27:02 +0900

    feat: Add src folder and completed absolute path configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['.'],
+        extensions: ['.js', '.ios.js', '.android.js', '.ts', '.tsx'],
+        alias: {
+          '~': './src',
+          '@components': './src/components',
+          '@elements': './src/elements',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
